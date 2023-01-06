9f1af6166709dbb89a2ec9a360253bc53244fe2c
rmne <faaa@live.com>
2023-01-06 22:59:01 +0800

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
