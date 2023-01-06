4deafe76e912894f81174bbe79c5652f5888c18d
rmne <faaa@live.com>
2023-01-06 15:41:32 +0800

    2 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -8,6 +10,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
