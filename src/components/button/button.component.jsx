fddc82d0cdffad0a98c384c5b577b1be9f2047ca
rmne <faaa@live.com>
2023-01-07 15:50:00 +0800

    1 file modified, 3 added

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './button.styles.scss';
+
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sing-in',
+  inverted: 'inverted',
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,7 +5,11 @@ import {
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
+import Button from '../button/button.component';
+
 import FormInput from '../form-input/form-input.component';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -54,8 +58,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -93,7 +98,9 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <button type="submit">Sign Up</button>
+        <Button buttonType="google" type="submit">
+          Sign Up
+        </Button>
       </form>
     </div>
   );
