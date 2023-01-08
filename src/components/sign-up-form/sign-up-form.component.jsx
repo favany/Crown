61f40cfadda77cb2b74ea1c56f0b5cfb0fd3ddc3
rmne <faaa@live.com>
2023-01-08 11:36:21 +0800

    1 file modified

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -22,7 +22,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
+  // console.log(formFields);
+  console.log('hit');
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
